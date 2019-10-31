ff02fce78193f0f597aa279a241ad372eb6a268a
bhaswanth97 <bhaswanth.nexevo@gmail.com>
2019-10-31 15:43:24 +0530

    Modified Controller

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,18 +5,11 @@
     {
         public function home()
         {
-            // return __DIR__ . "/views/home.php";
-            // return "../views/home.php";
-            // return $_SERVER['HTTP_HOST'] . "/projects/test/router/views/home.php";
-            // return "views/home.php";
             return $this->render("home");
-            // return $this->redirect("about/10/5/route/1540");
-            // echo env("APP_URL");
         }
 
         public function about($args)
         {
-            // return "views/about.php";
             $name = "Bhaswanth";
             $study = "10";
             $data = array('args' => $args, "name" => $name, "study" => $study);
